f842a7b72fd542a551c0566582461db435b9c8d7
vinigodoy <vinigodoy@gmail.com>
2011-01-12 20:34:16 -0200

    Added class description comment.

--- src/com/alegria2d/base/graphics/DisplayModeFinder.java
+++ src/com/alegria2d/base/graphics/DisplayModeFinder.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright (C) 2011 Alegria contributors
  * 
  * See CREDITS for information about contributors.
@@ -7,23 +7,27 @@
  * 
  * This file is part of Alegria 2D Java Game Framework.
  * 
- * Alegria is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Alegria is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
  * 
- * Alegria is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ * Alegria is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
  * 
- * You should have received a copy of the GNU Lesser General Public License
- * along with Alegria.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public License along with Alegria. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 package com.alegria2d.base.graphics;
 
 import java.util.Set;
 
+/**
+ * A DisplayModeFinder locates all display modes available in the local Graphics Environment. There is no support for
+ * multiple monitors yet.
+ * 
+ * @author Vinicius G. Mendonca
+ */
 public interface DisplayModeFinder {
    /**
     * @return the initial desktop display mode.
