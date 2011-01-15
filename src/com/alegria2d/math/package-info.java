dc3d9e2ba8de14924900bc5414ce218aad37a00e
vinigodoy <vinigodoy@gmail.com>
2011-01-15 19:02:58 -0200

    Added note to contributors.

--- src/com/alegria2d/math/package-info.java
+++ src/com/alegria2d/math/package-info.java
@@ -1,4 +1,11 @@
 /**
- * This package includes all Alegria mathematical classes. 
+ * This package includes all Alegria mathematical classes.
+ * <p>
+ * Note for contributors:
+ * <p>
+ * Some codes in this package may not follow the Java code conventions strictly. This package is an exception since
+ * there are also strong mathematical conventions, and sometimes they are preferred. An example of this is the
+ * {@link Matrix3D} class where some variables are declared uppercase, since matrices are represented in math as a
+ * uppercase letter.
  */
 package com.alegria2d.math;
